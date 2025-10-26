933a3fbbc2a363fec638883013d130221982d6ab
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-26 19:51:51 +0100

    Create on_disconnect.py

---
+++ lambda/on_disconnect.py
@@ -0,0 +1,8 @@
+import os, boto3
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
+def handler(event, context):
+    connection_id = event["requestContext"]["connectionId"]
+    table.delete_item(Key={"connectionId": connection_id})
+    return {"statusCode": 200, "body": "disconnected"}
