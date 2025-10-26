9e239e25150e76ff5a7408548d8a788d9757108a
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-26 21:23:18 +0100

    Update outputs.tf

--- files/outputs.tf
+++ files/outputs.tf
@@ -1,2 +1,14 @@
-output "api_id" { value = aws_apigatewayv2_api.ws.id }
-output "stage"  { value = var.stage }
+output "api_id" {
+  value = aws_apigatewayv2_api.ws.id
+}
+
+output "stage" {
+  value = var.stage
+}
+output "websocket_url" {
+  value = "wss://${replace(aws_apigatewayv2_api.ws.api_endpoint, "https://", "")}/${var.stage}"
+}
+
+output "manage_connections_arn" {
+  value = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.ws.id}/${var.stage}/POST/@connections/*"
+}
