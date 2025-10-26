ebff34db80212888a42da318a2e40902aa2c09bc
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-26 21:24:00 +0100

    Update variables.tf

--- files/variables.tf
+++ files/variables.tf
@@ -1,3 +1,17 @@
-variable "project" { default = "weatherws" }
-variable "region"  { default = "eu-north-1" }
-variable "stage"   { default = "prod" }
+variable "project" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "weatherws"
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "stage" {
+  description = "Deployment stage (e.g., dev, prod)"
+  type        = string
+  default     = "prod"
+}
